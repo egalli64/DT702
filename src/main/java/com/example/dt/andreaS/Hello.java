7c5057a9a80f21454b47fa29a7d7c8e7d1aed2ca
daedevils <daedevils@daedevils-ZenBook-UX433FN-UX433FN>
2022-02-04 09:55:19 +0100

    First AndreaS commit

---
+++ src/main/java/com/example/dt/andreaS/Hello.java
@@ -0,0 +1,13 @@
+package com.example.dt.andreaS;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public int multiplyFor(int value, int multiplier) {
+        return value * multiplier;
+    }
+}
+
