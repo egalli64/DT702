f560d3574eafc79169f716e65e70265842b51c22
Emanuele <egalli64@gmail.com>
2022-02-04 09:46:54 +0100

    first commit

---
+++ src/main/java/com/exemple/dt/emanuele/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.exemple.dt.emanuele.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public int multiplyFor(int value, int multiplier) {
+        return value * multiplier;
+    }
+}
---
+++ src/test/java/com/exemple/dt/emanuele/hello/HelloTest.java
@@ -0,0 +1,12 @@
+package com.exemple.dt.emanuele.hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+    @Test
+    void testMultiplyFor() {
+        fail("Not yet implemented");
+    }
+}
